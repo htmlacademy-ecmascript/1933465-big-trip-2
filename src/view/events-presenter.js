b1b5ff89978b56b6e4a1da9cc1816b5b07e70642
Evgeniy <krionic1@gmail.com>
2026-06-11 19:19:10 +0300

    Вносит мелкие исправления

--- src/view/events-presenter.js
+++ src/view/events-presenter.js
@@ -1,9 +1,10 @@
 import TripEventsListView from './trip-events-list-view.js';
 import EditPointView from './edit-point-view.js';
 import EventView from './event-view.js';
-import AddNewPointView from './add-new-point-view.js'
-
+import AddNewPointView from './add-new-point-view.js';
 import { render } from '../render.js';
+
+const TRIP_POINTS_COUNT = 3;
 
 export default class EventsPresenter {
   eventsListComponent = new TripEventsListView();
@@ -15,7 +16,7 @@ export default class EventsPresenter {
   init() {
     render(this.eventsListComponent, this.eventsContainer);
     render(new EditPointView(), this.eventsListComponent.getElement());
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < TRIP_POINTS_COUNT; i++) {
       render(new EventView(), this.eventsListComponent.getElement());
     }
     render(new AddNewPointView(), this.eventsListComponent.getElement());
--- src/view/trip-events-list-view.js
+++ src/view/trip-events-list-view.js
@@ -1,7 +1,7 @@
 import { createElement } from '../render.js';
 
 function createTripEventsListTemplate() {
-  return '<ul class="trip-events__list""></ul>';
+  return '<ul class="trip-events__list"></ul>';
 }
 
 export default class TripEventsListView {
